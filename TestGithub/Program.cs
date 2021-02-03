1bd1a40911d0af3a9790ba223e4724d8ea8d6856
Jason.Wu <wujpkimo@gmail.com>
2021-02-03 11:17:19 +0800

    加入專案檔案。

---
+++ TestGithub/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestGithub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

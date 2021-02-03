3467e2abc2928fdf123037039d01dd0360e20228
Jason.Wu <wujpkimo@gmail.com>
2021-02-03 11:18:36 +0800

    2021-02-03 Jason Add Hello byte

--- TestGithub/Program.cs
+++ TestGithub/Program.cs
@@ -2,11 +2,12 @@
 
 namespace TestGithub
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello BYTE");
         }
     }
 }
